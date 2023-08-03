b443fb2341990770f4c0f098c838c858230f8e6a
NOGUEN <nhg1113@naver.com>
2023-08-03 22:18:15 +0900

    feat: Main Page structure

--- lib/app/view/main_page.dart
+++ lib/app/view/main_page.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:one_day_one_something/app/core/base/base_view.dart';
+import 'package:one_day_one_something/app/view/common/goal/odos_add_button.dart';
 import 'package:one_day_one_something/app/view/common/system/odos_appbar.dart';
 import 'package:one_day_one_something/app/view/common/system/odos_bottom_navigationbar.dart';
-import 'package:one_day_one_something/app/view/common/system/odos_buttons.dart';
 import 'package:one_day_one_something/app/view/common/system/odos_system_list.dart';
-import 'package:one_day_one_something/app/view/common/system/odos_text_field.dart';
-import 'package:one_day_one_something/app/view/common/track/odos_track_card.dart';
-import 'package:one_day_one_something/app/view/common/goal/odos_progress_circle.dart';
-import 'package:one_day_one_something/app/view/common/goal/odos_week_button.dart';
-import 'package:one_day_one_something/app/view/common/goal/odos_add_button.dart';
-import 'package:one_day_one_something/app/view/common/goal/odos_my_goal.dart';
-import 'package:one_day_one_something/app/view/common/goal/odos_goal_list.dart';
 import 'package:one_day_one_something/app/view/theme/app_colors.dart';
 
 import '../controller/main/main_controller.dart';
+import '../data/model/enum/menu_code.dart';
 
 class MainPage extends BaseView<MainController> {
   @override
@@ -23,22 +18,38 @@ class MainPage extends BaseView<MainController> {
 
   @override
   Widget body(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(children: [
-        ODOSSystemList(list: [
-          ODOSSystemListCell(menuString: "설정"),
-          ODOSSystemListCell(menuString: "설정"),
-          ODOSSystemListCell(menuString: "설정"),
-          ODOSSystemListCell(menuString: "설정"),
-          ODOSSystemListCell(menuString: "설정"),
-          ODOSSystemListCell(menuString: "설정"),
-        ], categoryString: "설정")
-      ]),
+    return Container(
+      key: UniqueKey(),
+      child: Obx(() => getPageOnSelectedMenu(controller.selectedMenuCode)),
     );
   }
 
+  @override
   Widget? bottomNavigationBar() {
     return ODOSBottomNavigationBar(
         onNewMenuSelected: controller.onMenuSelected);
+  }
+
+  final Container homePage = Container();
+  Container? goalPage;
+  Container? settingPage;
+
+  Widget getPageOnSelectedMenu(MenuCode menuCode) {
+    switch (menuCode) {
+      case MenuCode.HOME:
+        // return homeView;
+        return Container();
+      case MenuCode.GOAL:
+        // return goalView;
+        goalPage ??= Container();
+        return goalPage!;
+      case MenuCode.MYPAGE:
+        // return goalView;
+        settingPage ??= Container();
+        return settingPage!;
+      default:
+        // return LoginPage();
+        return Container();
+    }
   }
 }
