0698cd49da67d21c3bd49a8b4a5bfeb3e25000b4
NOGUEN <nhg1113@naver.com>
2023-08-05 15:41:48 +0900

    feat: add LoginPageController #16

---
+++ lib/app/controller/auth/login_controller.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:one_day_one_something/app/core/base/base_controller.dart';
+
+import '../../routes/app_pages.dart';
+
+class LoginController extends BaseController {
+  final emailEditingController = TextEditingController();
+  final passwordEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailEditingController.dispose();
+    passwordEditingController.dispose();
+    super.dispose();
+  }
+
+  void login() {
+    showToast("로그인 성공 : ${emailEditingController.text}");
+    Get.offAndToNamed(Routes.MAIN);
+  }
+
+  void goRegister() {
+    Get.toNamed(Routes.REGISTER);
+  }
+}
