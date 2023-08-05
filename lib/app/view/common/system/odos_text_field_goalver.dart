0e2d67c4910d107796d8e06548d046c899f8cbf8
Jeon-jisu <wltnjeon0119@gmail.com>
2023-08-06 01:11:28 +0900

    feat: add textfield for goal add button (#4)

---
+++ lib/app/view/common/system/odos_text_field_goalver.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:one_day_one_something/app/view/theme/app_colors.dart';
+import 'package:one_day_one_something/app/view/theme/app_text_theme.dart';
+
+class ODOSTextGaolField extends StatelessWidget {
+  const ODOSTextGaolField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 280,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            TextField(
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: AppColors.gray700,
+                    width: 1,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: AppColors.black,
+                    width: 1,
+                  ),
+                ),
+                hintText: "제목",
+                hintStyle: inputGoalAddHintTextStyle,
+                contentPadding: EdgeInsets.fromLTRB(0, 8, 0, 0),
+              ),
+              style: inputContentTextStyle,
+            ),
+          ],
+        ));
+  }
+}
