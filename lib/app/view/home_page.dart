8bf27e6c7e9f1163fe8828ab6708119c2d7da219
eatingrabbit <lhy100494@gmail.com>
2023-08-12 21:30:40 +0900

    feat: modify EveryoneSTrackListSingleLine in home_page (#20)

--- lib/app/view/home_page.dart
+++ lib/app/view/home_page.dart
@@ -32,18 +32,20 @@ class HomePage extends BaseView<MainController> {
 class EveryoneSTrackListSingleLine extends StatelessWidget {
   final List trackList;
   final bool reverseScroll;
+  final int duration;
   const EveryoneSTrackListSingleLine({
     super.key,
     required this.trackList,
-    required this.reverseScroll
+    required this.reverseScroll,
+    required this.duration
   });
 
   @override
   Widget build(BuildContext context) {
     return ScrollLoopAutoScroll(
       delay: Duration(seconds: 0),
-      duration: Duration(seconds: 30),
-      duplicateChild: 4,
+      duration: Duration(seconds: duration * (trackList.length < 3 ? 12 : 6)),  //1개당 1.5초가 적당
+      duplicateChild: trackList.length < 3 ? 8 : 4,
       reverseScroll: reverseScroll,
       gap: 0,
       scrollDirection: Axis.horizontal,
@@ -126,16 +128,28 @@ class BaseHomePage extends StatelessWidget {
             children: [
               Transform.translate(
                 offset: Offset(0, -20),
-                child: EveryoneSTrackListSingleLine(trackList: everyoneSTrackList.sublist(0, 5), reverseScroll: true,)
+                child: EveryoneSTrackListSingleLine(
+                  trackList: everyoneSTrackList.sublist(0, min(everyoneSTrackList.length, 5)),
+                  reverseScroll: true,
+                  duration: min(5, (everyoneSTrackList.length-0)),
+                )
               ),
-              Transform.translate(
+              everyoneSTrackList.length > 5 ? Transform.translate(
                   offset: Offset(0, -40),
-                  child: EveryoneSTrackListSingleLine(trackList: everyoneSTrackList.sublist(5, 10), reverseScroll: false,)
-              ),
-              Transform.translate(
+                  child: EveryoneSTrackListSingleLine(
+                    trackList: everyoneSTrackList.sublist(5, min(everyoneSTrackList.length, 10)),
+                    reverseScroll: false,
+                    duration: min(5, (everyoneSTrackList.length-5)),
+                  )
+              ) : SizedBox.shrink(),
+              everyoneSTrackList.length > 10 ? Transform.translate(
                   offset: Offset(0, -60),
-                  child: EveryoneSTrackListSingleLine(trackList: everyoneSTrackList.sublist(10, min(everyoneSTrackList.length, 15)), reverseScroll: true,)
-              ),
+                  child: EveryoneSTrackListSingleLine(
+                    trackList: everyoneSTrackList.sublist(10, min(everyoneSTrackList.length, 15)),
+                    reverseScroll: true,
+                    duration: min(5, (everyoneSTrackList.length-10)),
+                  )
+              ) : SizedBox.shrink(),
             ],
           )
         ],
