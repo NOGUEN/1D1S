3790ea46a1f40beb027783ceaf991b94beca991d
eatingrabbit <lhy100494@gmail.com>
2023-08-15 22:31:26 +0900

    feat: modify home page with ListView

--- lib/app/view/home_page.dart
+++ lib/app/view/home_page.dart
@@ -80,84 +80,88 @@ class BaseHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       key: UniqueKey(),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: ListView(
         children: [
-          Padding(
-            //모두의 목표 버튼
-            padding: const EdgeInsets.fromLTRB(16, 21, 0, 0),
-            child: TextButton(
-              style: ButtonStyle(
-                overlayColor: MaterialStateColor.resolveWith(
-                    (states) => Colors.transparent)),
-              onPressed: () {},
-              child: Text(AppString.str_everyone_s_goal_button, style: everyoneSGoalButtonTextStyle),
-            ),
-          ),
-          SizedBox(
-            //모두의 목표 리스트
-            height: 140,
-            child: everyoneSGoalList.isEmpty ?
-            Center(
-              child: Text(AppString.str_empty_goal_list, style: everyoneSGoalButtonTextStyle,),
-            ) : ListView.builder(
-              scrollDirection: Axis.horizontal,
-              padding: EdgeInsets.fromLTRB(24, 20, 24, 20),
-              clipBehavior: Clip.none,
-              physics: ClampingScrollPhysics(),
-              itemCount: everyoneSGoalList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ODOSMyGoal(
-                        consecutive_days: everyoneSGoalList[index]
-                            ["consecutive_days"],
-                        my_goal: everyoneSGoalList[index]["my_goal"],
-                        imoji: 'images/icon_fire.png')
-                    .marginOnly(right: 17);
-              },
-            ),
-          ),
-          Padding(
-            //모두의 기록 버튼
-            padding: const EdgeInsets.fromLTRB(16, 21, 0, 20),
-            child: TextButton(
-              style: ButtonStyle(
-                  overlayColor: MaterialStateColor.resolveWith(
-                      (states) => Colors.transparent)),
-              onPressed: () {},
-              child: Text(AppString.str_everyone_s_track_button, style: everyoneSGoalButtonTextStyle),
-            ),
-          ),
-          everyoneSTrackList.isEmpty ?
-          Center(
-            child: Text(AppString.str_empty_track_list, style: everyoneSGoalButtonTextStyle,),
-          ) : Column(   //모두의 기록 리스트
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Transform.translate(
-                offset: Offset(0, -20),
-                child: EveryoneSTrackListSingleLine(
-                  trackList: everyoneSTrackList.sublist(0, min(everyoneSTrackList.length, 5)),
-                  reverseScroll: true,
-                  duration: min(5, (everyoneSTrackList.length-0)),
-                )
+              Padding(
+                //모두의 목표 버튼
+                padding: const EdgeInsets.fromLTRB(16, 21, 0, 0),
+                child: TextButton(
+                  style: ButtonStyle(
+                    overlayColor: MaterialStateColor.resolveWith(
+                        (states) => Colors.transparent)),
+                  onPressed: () {},
+                  child: Text(AppString.str_everyone_s_goal_button, style: everyoneSGoalButtonTextStyle),
+                ),
               ),
-              everyoneSTrackList.length > 5 ? Transform.translate(
-                  offset: Offset(0, -40),
-                  child: EveryoneSTrackListSingleLine(
-                    trackList: everyoneSTrackList.sublist(5, min(everyoneSTrackList.length, 10)),
-                    reverseScroll: false,
-                    duration: min(5, (everyoneSTrackList.length-5)),
-                  )
-              ) : SizedBox.shrink(),
-              everyoneSTrackList.length > 10 ? Transform.translate(
-                  offset: Offset(0, -60),
-                  child: EveryoneSTrackListSingleLine(
-                    trackList: everyoneSTrackList.sublist(10, min(everyoneSTrackList.length, 15)),
-                    reverseScroll: true,
-                    duration: min(5, (everyoneSTrackList.length-10)),
-                  )
-              ) : SizedBox.shrink(),
+              SizedBox(
+                //모두의 목표 리스트
+                height: 140,
+                child: everyoneSGoalList.isEmpty ?
+                Center(
+                  child: Text(AppString.str_empty_goal_list, style: everyoneSGoalButtonTextStyle,),
+                ) : ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  padding: EdgeInsets.fromLTRB(24, 20, 24, 20),
+                  clipBehavior: Clip.none,
+                  physics: ClampingScrollPhysics(),
+                  itemCount: everyoneSGoalList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ODOSMyGoal(
+                            consecutive_days: everyoneSGoalList[index]
+                                ["consecutive_days"],
+                            my_goal: everyoneSGoalList[index]["my_goal"],
+                            imoji: 'images/icon_fire.png')
+                        .marginOnly(right: 17);
+                  },
+                ),
+              ),
+              Padding(
+                //모두의 기록 버튼
+                padding: const EdgeInsets.fromLTRB(16, 21, 0, 20),
+                child: TextButton(
+                  style: ButtonStyle(
+                      overlayColor: MaterialStateColor.resolveWith(
+                          (states) => Colors.transparent)),
+                  onPressed: () {},
+                  child: Text(AppString.str_everyone_s_track_button, style: everyoneSGoalButtonTextStyle),
+                ),
+              ),
+              everyoneSTrackList.isEmpty ?
+              Center(
+                child: Text(AppString.str_empty_track_list, style: everyoneSGoalButtonTextStyle,),
+              ) : Column(   //모두의 기록 리스트
+                children: [
+                  Transform.translate(
+                    offset: Offset(0, -20),
+                    child: EveryoneSTrackListSingleLine(
+                      trackList: everyoneSTrackList.sublist(0, min(everyoneSTrackList.length, 5)),
+                      reverseScroll: true,
+                      duration: min(5, (everyoneSTrackList.length-0)),
+                    )
+                  ),
+                  everyoneSTrackList.length > 5 ? Transform.translate(
+                      offset: Offset(0, -40),
+                      child: EveryoneSTrackListSingleLine(
+                        trackList: everyoneSTrackList.sublist(5, min(everyoneSTrackList.length, 10)),
+                        reverseScroll: false,
+                        duration: min(5, (everyoneSTrackList.length-5)),
+                      )
+                  ) : SizedBox.shrink(),
+                  everyoneSTrackList.length > 10 ? Transform.translate(
+                      offset: Offset(0, -60),
+                      child: EveryoneSTrackListSingleLine(
+                        trackList: everyoneSTrackList.sublist(10, min(everyoneSTrackList.length, 15)),
+                        reverseScroll: true,
+                        duration: min(5, (everyoneSTrackList.length-10)),
+                      )
+                  ) : SizedBox.shrink(),
+                ],
+              )
             ],
-          )
+          ),
         ],
       ),
     );
