1997493b674bea92180954e809fc263cb8f3a4c9
NOGUEN <nhg1113@naver.com>
2023-08-18 16:29:14 +0900

    merge

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -3,12 +3,12 @@
 import 'package:get/get.dart';
 import 'package:one_day_one_something/app/view/auth/login_page.dart';
 import 'package:one_day_one_something/app/view/auth/register_page.dart';
+import 'package:one_day_one_something/app/view/main_page/main_page.dart';
 import 'package:one_day_one_something/app/view/setting/setting_page.dart';
 import 'package:one_day_one_something/app/view/update_profile_page.dart';
-
 import '../bindings/auth_binding.dart';
 import '../bindings/main_binding.dart';
-import '../view/main_page.dart';
+
 part './app_routes.dart';
 
 // 앱 내 페이지를 정의하는 공간, 페이지 만든 후 여기에 정의해주세요
--- lib/app/view/main_page/goal_page.dart
+++ lib/app/view/main_page/goal_page.dart
@@ -15,7 +15,6 @@ class GoalPage extends BaseView<MainController> {
   @override
   PreferredSizeWidget? appBar(BuildContext context) {
     return null;
-    throw UnimplementedError();
   }
 
   @override
@@ -26,49 +25,78 @@ class GoalPage extends BaseView<MainController> {
       child: ListView(
         clipBehavior: Clip.none,
         padding: EdgeInsets.all(20),
-
-        children: [Center(
-          child: Wrap(
-            direction: Axis.vertical,
-            spacing: 20,
-            children: [
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.only(right: 8),
-                      child: CircleAvatar(
-                          radius: 20,
-                          backgroundColor: Colors.white,
-                          backgroundImage: AssetImage("images/image_user_profile_gorani.jpg")
+        children: [
+          Center(
+            child: Wrap(
+              direction: Axis.vertical,
+              spacing: 20,
+              children: [
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(right: 8),
+                        child: CircleAvatar(
+                            radius: 20,
+                            backgroundColor: Colors.white,
+                            backgroundImage: AssetImage(
+                                "images/image_user_profile_gorani.jpg")),
                       ),
-                    ),
-                    Text("고라니's 목표", style: profileCardHead,)
-                  ],
+                      Text(
+                        "고라니's 목표",
+                        style: profileCardHead,
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              ODOSCalander(dateOffset: 6, dateNum: 31),
-              Text("내 목표", style: head2,),
-              ODOSGoalList(imoji: "images/icon_fire.png", circleColor: AppColors.greenBackground, my_goal: "목표", percent: .375),
-              ODOSGoalList(imoji: "images/icon_fire.png", circleColor: AppColors.orangeBackground, my_goal: "목표", percent: .5),
-              ODOSGoalList(imoji: "images/icon_fire.png", circleColor: AppColors.redBackground, my_goal: "목표", percent: .75),
-              ODOSGoalList(imoji: "images/icon_fire.png", circleColor: AppColors.blueBackground, my_goal: "목표", percent: 1),
-              ODOSAddButton(buttonColor: Colors.black),
-              TextButton(
-                onPressed: () {
-                  showDialog(context: context, builder: (context) {
-                    return Dialog(child: ODOSGoalList(imoji: "images/icon_fire.png", circleColor: AppColors.redBackground, my_goal: "목표", percent: 1));
-                  });
-                },
-                child: Text('hi'),
-              )
-            ],
-          ),
-        )],
+                ODOSCalander(dateOffset: 6, dateNum: 31),
+                Text(
+                  "내 목표",
+                  style: head2,
+                ),
+                ODOSGoalList(
+                    imoji: "images/icon_fire.png",
+                    circleColor: AppColors.greenBackground,
+                    my_goal: "목표",
+                    percent: .375),
+                ODOSGoalList(
+                    imoji: "images/icon_fire.png",
+                    circleColor: AppColors.orangeBackground,
+                    my_goal: "목표",
+                    percent: .5),
+                ODOSGoalList(
+                    imoji: "images/icon_fire.png",
+                    circleColor: AppColors.redBackground,
+                    my_goal: "목표",
+                    percent: .75),
+                ODOSGoalList(
+                    imoji: "images/icon_fire.png",
+                    circleColor: AppColors.blueBackground,
+                    my_goal: "목표",
+                    percent: 1),
+                ODOSAddButton(buttonColor: Colors.black),
+                TextButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return Dialog(
+                              child: ODOSGoalList(
+                                  imoji: "images/icon_fire.png",
+                                  circleColor: AppColors.redBackground,
+                                  my_goal: "목표",
+                                  percent: 1));
+                        });
+                  },
+                  child: Text('hi'),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
-
 }
