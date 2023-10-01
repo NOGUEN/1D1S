df1659929eb619bbccee33c2778d2d10ce1afb0a
Jeon-jisu <wltnjeon0119@gmail.com>
2023-10-01 23:29:19 +0900

    feat: change firebase model(user, goal) for firestore database

--- lib/app/data/model/firebase/user_model.dart
+++ lib/app/data/model/firebase/user_model.dart
@@ -1,7 +1,7 @@
 import 'package:one_day_one_something/app/data/model/firebase/goal_model.dart';
 import 'package:one_day_one_something/app/data/model/firebase/track_model.dart';
 import 'package:json_annotation/json_annotation.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 part 'user_model.g.dart';
 
 @JsonSerializable()
@@ -26,8 +26,32 @@ class UserModel {
     this.friendTrackList,
   });
 
-  factory UserModel.fromJson(Map<String, dynamic> json) =>
-      _$UserModelFromJson(json);
+  // factory UserModel.fromJson(Map<String, dynamic> json) =>
+  //     _$UserModelFromJson(json);
 
-  Map<String, dynamic> toJson() => _$UserModelToJson(this);
+  // Map<String, dynamic> toJson() => _$UserModelToJson(this);
+  factory UserModel.fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
+    final data = snapshot.data();
+    return UserModel(
+        uid: data?['uid'],
+        nickname: data?['nickname'],
+        name: data?['name'],
+        email: data?['email']);
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      if (uid != null) "uid": uid,
+      "nickname": nickname,
+      "name": name,
+      "email": email,
+      if (imageUrl != null) "imageUrl": imageUrl,
+      if (goalList != null) "goalList": goalList,
+      if (friendUidList != null) "friendUidList": friendUidList,
+      if (friendTrackList != null) "friendTrackList": friendTrackList,
+    };
+  }
 }
