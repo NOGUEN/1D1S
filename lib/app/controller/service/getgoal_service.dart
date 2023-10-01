2e6eb6a42e0c81d16f01b5d0cfdf6534226affe0
Jeon-jisu <wltnjeon0119@gmail.com>
2023-10-01 23:51:51 +0900

    feat: make change readGoal example tmp

--- lib/app/controller/service/getgoal_service.dart
+++ lib/app/controller/service/getgoal_service.dart
@@ -1,14 +1,20 @@
 import 'package:one_day_one_something/app/data/local/db/storage_util.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:one_day_one_something/app/data/model/firebase/goal_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final FirebaseFirestore db = FirebaseFirestore.instance;
 
 class GoalService with StorageUtil {
-  Future<Datasnapshot> readGoal({required String userId}) async 
-    DatabaseReferece ref = FirebaseDatabase.instance.ref("$userId/user_data/");
-    final snapshot = await ref.child("").get();
-    return snapshot;
+  void readGoal() async {
+    await db.collection("users").get().then((event) {
+      for (var doc in event.docs) {
+        print("${doc.id} => ${doc.data()}");
+      }
+    });
   }
 
-  void addGoal({required GoalModel goal}){
-    DatabaseReferece ref = FirebaseDatabase.instance.ref
-  }
+  void addGoal({required GoalModel goal}) {}
+  void deleteGoal({required GoalModel goal}) {}
+  void updateGoal({required GoalModel goal}) {}
 }
