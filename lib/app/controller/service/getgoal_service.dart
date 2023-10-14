7d357a24b5dadca9aa370bd6be4a855a3f044005
Jeon-jisu <wltnjeon0119@gmail.com>
2023-10-14 17:42:32 +0900

    feat: add getgoal_service base code #43

--- lib/app/controller/service/getgoal_service.dart
+++ lib/app/controller/service/getgoal_service.dart
@@ -3,6 +3,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:one_day_one_something/app/data/model/firebase/goal_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+
 final FirebaseFirestore db = FirebaseFirestore.instance;
 
 class GoalService with StorageUtil {
@@ -13,8 +14,38 @@ class GoalService with StorageUtil {
       }
     });
   }
+  void addGoal(String? uid,{required GoalModel goal})async{
+    await db
+      .collection('users')
+      .doc(uid)
+      .collection("goallist")
+      .doc()
+      .set({
+        "update":true,
+        "modify":false,
+      });
+    
+  }
+  void deleteGoal(String? uid,{required GoalModel goal}) async{
+    await db
+      .collection("users")
+      .doc(uid)
+      .update({
+        "update":false
+      });
 
-  void addGoal({required GoalModel goal}) {}
-  void deleteGoal({required GoalModel goal}) {}
-  void updateGoal({required GoalModel goal}) {}
+  }
+  void updateGoal(String? uid,{required GoalModel goal}) async{
+    DocumentSnapshot<Map<String, dynamic>> _data = await db.collection("users").doc(uid).get();
+    Map<String, dynamic> _newData = {
+	    "update": _data.data()!["update"],
+	    "modify": true
+	};
+    await db
+      .collection("users")
+      .doc(uid)
+      .update(
+        _newData
+      );
+  }
 }
