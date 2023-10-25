fb298a764db678e473a76cf93ab411cf5cc9c37a
Jeon-jisu <wltnjeon0119@gmail.com>
2023-10-25 15:55:11 +0900

    feat: implement changing goal_complement if goal_days == goal_streak #43

--- lib/app/controller/service/record_service.dart
+++ lib/app/controller/service/record_service.dart
@@ -7,50 +7,63 @@ class RecordController {
   var record_date = DateTime.now().toString().obs;
   var record_note = ''.obs;
 
-  void saveDataToFirestore(String docId, DateTime date) { //새롭게 데이터 추가
+  void saveDataToFirestore(String docId, DateTime date) {
+    //새롭게 데이터 추가
     FirebaseFirestore firestore = FirebaseFirestore.instance;
     CollectionReference users = firestore.collection('users');
     final userid = FirebaseAuth.instance.currentUser!.uid;
-    
+
     DocumentReference userDocRef = users.doc(userid);
-    CollectionReference records = userDocRef
-      .collection('goallist')
-      .doc(docId)
-      .collection('record_list');
-    
-    records.add({
-      'record_date': record_date.value,
-      'record_note': record_note.value,
-    })
-    .then((value) => print("Record added to Firestore"))
-    .catchError((error) => print("Failed to add record: $error"));
+    CollectionReference records =
+        userDocRef.collection('goallist').doc(docId).collection('record_list');
+
+    records
+        .add({
+          'record_date': record_date.value,
+          'record_note': record_note.value,
+        })
+        .then((value) => print("Record added to Firestore"))
+        .catchError((error) => print("Failed to add record: $error"));
   }
 
-  void increaseStreak(String docId) async{
+  void increaseStreak(String docId) async {
     FirebaseFirestore firestore = FirebaseFirestore.instance;
     CollectionReference users = firestore.collection('users');
     final userid = FirebaseAuth.instance.currentUser!.uid;
-    
-    DocumentReference userDocRef = users.doc(userid);
-    DocumentSnapshot<Map<String, dynamic>> record_data = await userDocRef
-      .collection('goallist')
-      .doc(docId).get();
 
-    Map<String, dynamic> new_record_data = {
+    DocumentReference userDocRef = users.doc(userid);
+    DocumentSnapshot<Map<String, dynamic>> record_data =
+        await userDocRef.collection('goallist').doc(docId).get();
+    Map<String, dynamic> new_record_data;
+    Map<String, dynamic> origin_record_data = {
       "goal_color": record_data.data()!["goal_color"],
       "goal_complete": record_data.data()!["goal_complete"],
       "goal_days": record_data.data()!["goal_days"],
       "goal_emoji": record_data.data()!["goal_emoji"],
       "goal_name": record_data.data()!["goal_name"],
-      "goal_streak":record_data.data()!["goal_streak"]+1
+      "goal_streak": record_data.data()!["goal_streak"]
     };
-    await userDocRef
-      .collection('goallist')
-      .doc(docId)
-      .update(
-        new_record_data
-      );
+    if (origin_record_data["goal_streak"] ==
+        origin_record_data["goal_days"] - 1) //record_list가 추가되면 complete로 바꿔준다.
+    {
+      new_record_data = {
+        "goal_color": record_data.data()!["goal_color"],
+        "goal_complete": true,
+        "goal_days": record_data.data()!["goal_days"],
+        "goal_emoji": record_data.data()!["goal_emoji"],
+        "goal_name": record_data.data()!["goal_name"],
+        "goal_streak": record_data.data()!["goal_streak"] + 1
+      };
+    } else {
+      new_record_data = {
+        "goal_color": record_data.data()!["goal_color"],
+        "goal_complete": record_data.data()!["goal_complete"],
+        "goal_days": record_data.data()!["goal_days"],
+        "goal_emoji": record_data.data()!["goal_emoji"],
+        "goal_name": record_data.data()!["goal_name"],
+        "goal_streak": record_data.data()!["goal_streak"] + 1
+      };
+    }
+    await userDocRef.collection('goallist').doc(docId).update(new_record_data);
   }
-
-  
 }
