6fc623c105da1482109467d1393923e07d938800
Jeon-jisu <wltnjeon0119@gmail.com>
2023-10-25 15:26:44 +0900

    feat: separate enum from goal_page.dart (image path, dayEnum) #43

---
+++ lib/app/data/firebase/constants/constants.dart
@@ -0,0 +1,14 @@
+const List<String> imagepath = [
+  'images/profile_0.png',
+  'images/profile_1.png',
+  'images/profile_2.png',
+  'images/profile_3.png',
+  'images/moonshot.png',
+  'images/profile_4.png',
+  'images/profile_5.png',
+  'images/profile_6.png',
+  'images/profile_7.png',
+  "images/image_user_profile_gorani.jpg"
+];
+
+const List<int> dayEnum = [5, 7, 10, 20, 30, 50, 100];
--- lib/app/view/main_page/goal_page.dart
+++ lib/app/view/main_page/goal_page.dart
@@ -12,22 +12,7 @@ import 'package:one_day_one_something/app/view/common/system/odos_appbar.dart';
 import 'package:one_day_one_something/app/view/theme/app_colors.dart';
 import 'package:one_day_one_something/app/view/theme/app_text_theme.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
-List<String> imagepath = [
-  'images/profile_0.png',
-  'images/profile_1.png',
-  'images/profile_2.png',
-  'images/profile_3.png',
-  'images/moonshot.png',
-  'images/profile_4.png',
-  'images/profile_5.png',
-  'images/profile_6.png',
-  'images/profile_7.png',
-  "images/image_user_profile_gorani.jpg"
-
-  // Add more emoji URLs or paths as needed
-];
-List<int> dayEnum = [5, 7, 10, 20, 30, 50, 100];
+import 'package:one_day_one_something/app/data/firebase/constants/constants.dart';
 
 class GoalPage extends BaseView<MainController> {
   @override
@@ -86,7 +71,7 @@ class GoalPage extends BaseView<MainController> {
                   percent = (eachgoal['goal_streak'] /
                       dayEnum[eachgoal['goal_days']]);
                 } else {
-                  percent = 0.0; // or set to any default value if needed
+                  percent = 0.0;
                 }
 
                 final goalwidget = ODOSGoalList(
